dc7d25808d582eaacf2d9effa07c43c5be5d33ad
ColeDavis213 <coledavi@ut.utm.edu>
2018-11-24 22:54:26 -0600

    Changed main to sum calculator

--- main.cpp
+++ main.cpp
@@ -2,7 +2,24 @@
 
 using namespace std;
 
+int sum(int n)
+{
+  int sum = 0;
+  
+  for(int i = 1; i <= n; i++)
+    {
+      sum += i;
+    }
+
+  return sum;
+}
+
 int main()
 {
-  cout << "Hello World!" << endl;
+  int n;
+
+  cout << "Enter a number: ";
+  cin >> n;
+
+  cout << "Sum: " << sum(n) << endl;
 }
